691d805e3161db702466e0f47226df220019f0cc
Arpit <arpit_jain@live.com>
2022-09-16 15:25:19 +0530

    life cycle methods, state, props

--- todoapp/src/components/app/app.jsx
+++ todoapp/src/components/app/app.jsx
@@ -1,14 +1,54 @@
-import { Component } from 'react';
-import { Todos } from '../todos/todos';
-import './app.css';
+import React, { Component } from "react";
+import { Todos } from "../todos/todos";
+import "./app.css";
+
+// Life cycle methods
+// mounting
+  // constructor
+  // static getDerivedStateFromProps // create a state based on received props
+  // render
+  // componentDidMount // cuase side-effects // calling API, subscribing to a service, listening to an event (scroll event on body, window resize), updating comp state
+// update // whenever there is a change in state or props
+  // static getDerviedStateFromProps // create a state based on received props
+  // shouldComponentUpdate // decision point return boolean
+  // render
+  // static getSnapshotBeforeUpdate
+  // componentDidUpdate
+// unmounting
+  // componentWillUnmount // remove event listeners, unsubscribe from services
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('APP', props);
+    this.name = "App";
+    this.state = {
+      appName: 'todoapp'
+    }
+  }
+
+  /* componentDidMount() {
+    console.log(
+      "componentDidMount, I get called only once. I get called when the component is created and attached to the dom."
+    );
+  }
+
+  componentWillUnmount() {
+    console.log(
+      "componentWillUnmount, I get called only once. I get called when the component is detached from the dom and destroyed."
+    );
+  }
+
+  shouldComponentUpdate() {}
+
+  static getDerivedStateFromSnapshot() {} */
+
   render() {
     return (
       <div className="app">
         <h1 className="app-title">TODOAPP</h1>
-        <Todos />
+        <Todos  />
       </div>
-    )
+    );
   }
 }
--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App name='Todoapp' version="1.0.0" />
   </React.StrictMode>
 );
 
